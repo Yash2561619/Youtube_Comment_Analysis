a9f194d0c177616b02fc4674683ef2c3e9b6fe23
Yash Kabure <yashkabure@gmail.com>
2026-08-03 16:33:10 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='YouTube comment sentiment analysis using machine learning',
+    author='Yash',
+    license='',
+)
